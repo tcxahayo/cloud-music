ab18d6fdfc06f8bbd5f966e24b4077432e6387ee
tcxahayo <1392091646@qq.com>
2020-06-05 18:51:22 +0800

    1

---
+++ src/assets/iconfont/iconfont.js
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from 'styled-components';
+export const IconStyle = createGlobalStyle`
+    @font-face {
+        font-family: 'iconfont';  /* project id 1866268 */
+        src: url('//at.alicdn.com/t/font_1866268_isbmu5me22.eot');
+        src: url('//at.alicdn.com/t/font_1866268_isbmu5me22.eot?#iefix') format('embedded-opentype'),
+        url('//at.alicdn.com/t/font_1866268_isbmu5me22.woff2') format('woff2'),
+        url('//at.alicdn.com/t/font_1866268_isbmu5me22.woff') format('woff'),
+        url('//at.alicdn.com/t/font_1866268_isbmu5me22.ttf') format('truetype'),
+        url('//at.alicdn.com/t/font_1866268_isbmu5me22.svg#iconfont') format('svg');
+    }
+    .iconfont{
+        font-family:"iconfont" !important;
+        font-size:16px;font-style:normal;
+        -webkit-font-smoothing: antialiased;
+        -webkit-text-stroke-width: 0.2px;
+        -moz-osx-font-smoothing: grayscale;}
+  
+`;
