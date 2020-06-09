24538a1208684d02f039f8c496d231e33280afbc
tcxahayo <1392091646@qq.com>
2020-06-09 19:14:48 +0800

    3

---
+++ src/api/util.js
@@ -0,0 +1,20 @@
+/**
+ * 判断是否为空，空格，空字符串， undefined,长度为0的数组、对象都为空
+ * @param {*} value 
+ */
+export const isEmpty =(value)=>{
+    console.log(value)
+    if(value === undefined || value === '' || value === null){
+        return true
+    }
+    if(typeof(value) === 'string'){
+        return !Boolean(value.trim());//去掉空格后，是否为空
+    }
+    if(typeof(value) === 'object'){
+        for(let i in value){
+            return false
+        }
+        return true
+    }
+    return false
+}
---
+++ src/components/slider/index.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from 'react';
+import { isEmpty } from '../../api/util';
+import Swiper from 'swiper';
+import "swiper/css/swiper.css";
+import { SliderContainer } from './style';
+
+function Slider(props) {
+    const [exist, setExist] = useState(null);
+    const { bannerList } = props;
+    useEffect(() => {
+        if (!isEmpty(bannerList) && isEmpty(exist)) {
+            let newSliderSwiper = new Swiper(".slider-container", {
+                loop: true,
+                autoplay: {
+                    delay: 3000,
+                    disableOnInteraction: false,
+                },
+                //分页小圆点
+                pagination: {
+                    el:'.swiper-pagination'
+                }
+            });
+            setExist('newSliderSwiper')
+        }
+    },[bannerList,exist])
+
+    return (
+        <SliderContainer>
+            <div className='slider-container'>
+                <div className='swiper-wrapper'>
+                    {
+                        bannerList.map((item,index) => {
+                            return (
+                            <div className='swiper-slide' key={index}>
+                                <div className='slider-nav'>
+                                    <img src={item.imageUrl} width="100%" height="100%" alt='推荐' />
+                                </div>
+                            </div>
+                            )
+                        })
+                    }
+                </div>
+                <div className='swiper-pagination'></div>
+            </div>
+        </SliderContainer>
+    )
+}
+export default React.memo(Slider);
